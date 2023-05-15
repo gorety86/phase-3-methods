2b06be1023ebd4177e493cfb6eebcbaddf392508
gorety <wagutugorety@gmail.com>
2023-05-15 16:06:17 +0300

    assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,24 @@
 # Your code here!
+def greet_programmer()
+    puts "Hello, programmer!"
+
+end
+
+def greet(name = "Naureen")
+    puts "Hello, #{name}!"
+
+end
+
+def greet_with_default(name="programmer")
+    puts "Hello, #{name}!"
+end
+
+def add(num1, num2)
+    return num1 + num2
+
+end
+
+def halve(x)
+    return x/2
+    
+end
